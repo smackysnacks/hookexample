9598099d3254221af4d0c63ab14a5291b9728bdd
Mack Stump <mack@mackstump.com>
2019-03-21 22:12:05 -0400

    pointless circle!

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+use std::ops::{Deref, DerefMut};
 use std::sync::Mutex;
 
 use detour::GenericDetour;
 use lazy_static::lazy_static;
 use log::info;
 use widestring::{U16CStr, U16CString};
-use winapi::ctypes::wchar_t;
+use winapi::ctypes::{c_char, wchar_t};
 use winapi::shared::minwindef::*;
 use winapi::um::winnt::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH};
 
@@ -51,9 +52,21 @@ macro_rules! disable_hook {
 
 // Function signatures
 type ConsoleWriteFn = extern "cdecl" fn(TextColor, *const wchar_t) -> BOOL;
+type SpawnItemFn = extern "cdecl" fn(*const c_char) -> u32;
 
 // Hook setup
-setup_hook!(DETOUR_CONSOLE_WRITE, ConsoleWriteFn, 0x00450b90, detour_console_write);
+setup_hook!(
+    DETOUR_CONSOLE_WRITE,
+    ConsoleWriteFn,
+    0x00450b90,
+    detour_console_write
+);
+setup_hook!(
+    DETOUR_SPAWN_ITEM,
+    SpawnItemFn,
+    0x004e3810,
+    detour_spawn_item
+);
 
 lazy_static! {
     static ref LAST_MESSAGE_HAX: Mutex<bool> = Mutex::new(false);
@@ -106,12 +119,115 @@ extern "cdecl" fn detour_console_write(color: TextColor, message: *const wchar_t
                 let u16cstring = U16CString::from_str(line).unwrap();
                 realfn(TextColor::LightBlue, u16cstring.as_ptr());
             });
+        } else if &s[6..] == " dump entities" {
+            dump_map_entities();
+        } else if &s[6..] == " circle" {
+            circle();
         }
 
         return TRUE;
     }
 
     return realfn(color, message);
+}
+
+#[derive(Debug)]
+struct PlayerCircle {
+    origin: (f32, f32), // x, y
+    radius: f32,
+}
+
+struct UnsafeEntity(&'static mut Entity);
+unsafe impl Send for UnsafeEntity {}
+
+impl Deref for UnsafeEntity {
+    type Target = Entity;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for UnsafeEntity {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+fn circle() {
+    let mut player = UnsafeEntity(get_player_entity());
+    let pc = PlayerCircle {
+        origin: (player.xcoord, player.ycoord),
+        radius: 100.0,
+    };
+
+    std::thread::spawn(move || {
+        for i in 0.. {
+            let x = pc.origin.0 + pc.radius * ((i % 360) as f32 * 0.01745).cos();
+            let y = pc.origin.1 + pc.radius * ((i % 360) as f32 * 0.01745).sin();
+
+            player.xcoord = x;
+            player.ycoord = y;
+
+            std::thread::sleep(std::time::Duration::from_millis(3000 / 360));
+        }
+    });
+}
+
+extern "cdecl" fn detour_spawn_item(itemname: *const c_char) -> u32 {
+    let realfn: SpawnItemFn =
+        unsafe { std::mem::transmute(DETOUR_SPAWN_ITEM.lock().unwrap().trampoline()) };
+
+    let item = unsafe { std::ffi::CString::from_raw(std::mem::transmute(itemname)) };
+    info!("spawn_item called with '{}'", item.to_string_lossy());
+    std::mem::forget(item);
+
+    return realfn(itemname);
+}
+
+#[repr(C)]
+struct Entity {
+    // +0x00
+    unk1: [u8; 0x28],
+    // +0x28
+    extent: u16,
+    // +0x2a
+    unk2: [u8; 0x16],
+    // +0x40
+    xcoord: f32,
+    // +0x44
+    ycoord: f32,
+    // +0x48
+    unk3: [u8; 0x194],
+    // +0x1dc
+    next_entity: *mut Entity,
+}
+
+fn get_player_entity() -> &'static mut Entity {
+    let mut entity: &mut Entity = unsafe { &mut **(0x00750708 as *const *mut Entity) };
+    while entity.next_entity != 0 as *mut Entity && entity.extent != 0 {
+        entity = unsafe { &mut *entity.next_entity };
+    }
+
+    entity
+}
+
+fn dump_map_entities() {
+    let mut entity: &mut Entity = unsafe { &mut **(0x00750708 as *const *mut Entity) };
+    info!(
+        "entity = &entity: {:?}, extent: {}, xcoord: {}, ycoord: {}",
+        entity as *const _, entity.extent, entity.xcoord, entity.ycoord
+    );
+    while entity.next_entity != 0 as *mut Entity {
+        entity = unsafe { &mut *entity.next_entity };
+        info!(
+            "entity = &entity: {:?}, extent: {}, xcoord: {}, ycoord: {}",
+            entity as *const _, entity.extent, entity.xcoord, entity.ycoord
+        );
+        if entity.extent == 0 {
+            entity.xcoord += 50.0;
+        }
+    }
 }
 
 fn on_attach() {
@@ -125,6 +241,7 @@ fn on_attach() {
 
     // Enable hooks
     enable_hook!(DETOUR_CONSOLE_WRITE);
+    enable_hook!(DETOUR_SPAWN_ITEM);
 }
 
 fn on_detach() {
@@ -132,6 +249,7 @@ fn on_detach() {
 
     // Disable hooks
     disable_hook!(DETOUR_CONSOLE_WRITE);
+    disable_hook!(DETOUR_SPAWN_ITEM);
 
     // Detach the console
     unsafe { winapi::um::wincon::FreeConsole() };
